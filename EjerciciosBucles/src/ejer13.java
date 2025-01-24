7f0b9d73240849eeadb56ba06210b11d6de367dc
Diego Lizana <helgait@usal.es>
2025-01-24 09:59:13 +0100

    solved loop exercise

---
+++ EjerciciosBucles/src/ejer13.java
@@ -0,0 +1,20 @@
+//(L2) 3. Muestra por pantalla todos los números primos entre 2 y 100, ambos incluidos.
+
+public class ejer13 {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++){
+            boolean primo = true;
+
+            for(int j=2; j<i; j++){
+                if(i%j==0){
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo){
+                System.out.println(i);
+            }
+        }
+    }
+}
