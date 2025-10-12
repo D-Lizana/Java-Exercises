3c6d4a77db703163d7f8e66497de1bae0c3b0737
DLizanaDev <Helgait@usal.es>
2025-10-12 18:38:29 +0200

    Added folder for Acceso a Datos lessons and started with reader and writer exercises

--- Acceso a Datos/src/EjercicioSupermercado.java
+++ Acceso a Datos/src/EjercicioSupermercado.java
@@ -1,12 +1,16 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class EjercicioSupermercado {
 
     public static void main(String[] args) {
 
         try{
-            crearFichero("Supermercado");
-            leerArchivo("Supermercado");
+            crearFichero("Supermercado.txt");
+            escribirArchivoBufferedWriter("Supermercado.txt", "Martillo", 30, 10.00);
+            leerArchivoBufferedReader("Supermercado.txt");
         }catch (IOException e){
             e.printStackTrace();
         }
@@ -26,7 +30,45 @@ public class EjercicioSupermercado {
 
     }
 
-    public static void leerArchivo(String archivo){
+    public static void borrarFichero(String nombreArchivo){
+        File archivo = new File(nombreArchivo);
+
+        if(archivo.exists()){
+            if(archivo.delete()){
+                System.out.println("Archivo borrado.");
+            }
+            else{
+                System.out.println("No se ha podido borrar el archivo.");
+            }
+
+        }
+        else {
+            System.out.println("El archivo no existe.");
+        }
+    }
+
+    public static void copiarArchivo(String origen, String destino){
+        File archivoOrigen = new File((origen));
+        File archivoDestino = new File((destino));
+
+        if(!archivoOrigen.exists() || !archivoOrigen.isFile()){
+            System.out.println("El archivo de origen no existe o no es un archivo válido");
+            return;
+        }
+
+        Path rutaOrigen = archivoOrigen.toPath();
+        Path rutaDestino = archivoDestino.toPath();
+
+        try{
+            Files.copy(rutaOrigen,rutaDestino, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Archivo copiado con éxito.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void leerArchivoBufferedReader(String archivo){
 
         try{
             BufferedReader br = new BufferedReader(new FileReader(archivo));
@@ -42,18 +84,73 @@ public class EjercicioSupermercado {
         }
     }
 
-    public static void escribirArchivo(String archivo, String texto){
+    public static void escribirFileWriter(String archivo, String texto){
         try{
-            FileWriter fw = new FileWriter(archivo, true);
+            FileWriter fw = new FileWriter(archivo); // Sobrescribe el archivo
             fw.write(texto);
-            fw.write((System.lineSeparator()));
-            System.out.println("texto añadido correctamente.");
+            fw.write(System.lineSeparator());
+            fw.close();
+            System.out.println("Texto añadido correctamente.");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void anadirFileWriter(String archivo, String texto){
+        try{
+            FileWriter fw = new FileWriter(archivo, true); // Sobrescribe el archivo
+            fw.write(texto);
+            fw.write(System.lineSeparator());
+            fw.close();
+            System.out.println("Texto añadido correctamente.");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void escribirArchivoBufferedWriter(String archivo, String texto){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            System.out.println("Texto añadido correctamente.");
 
 
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
+    public static void anadirArchivoBufferedWriter(String archivo, String texto){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true));
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            System.out.println("Texto añadido correctamente.");
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void anadirPrintWriter(String archivo, String texto){
+        try{
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true));
+            bw.write(texto);
+            bw.newLine();
+            bw.close();
+            System.out.println("Texto añadido correctamente.");
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 }
