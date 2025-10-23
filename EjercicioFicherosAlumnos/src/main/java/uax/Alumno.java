e7dd71a45ff47d312c518fac7895267443c3526f
DLizanaDev <Helgait@usal.es>
2025-10-23 12:27:41 +0200

    Added methods and constructors

--- EjercicioFicherosAlumnos/src/main/java/uax/Alumno.java
+++ EjercicioFicherosAlumnos/src/main/java/uax/Alumno.java
@@ -7,13 +7,27 @@ public class Alumno {
     private String nombreYApellidos;
     private double nota;
 
+    public Alumno() {
+    }
+
     public Alumno(int expediente, String nombreYApellidos) {
         this.expediente = expediente;
         this.nombreYApellidos = nombreYApellidos;
     }
 
+    public void setNota(double nota){
+        this.nota = nota;
+    }
 
+    public int getExpediente() {
+        return expediente;
+    }
 
+    public String getNombreYApellidos() {
+        return nombreYApellidos;
+    }
 
-
+    public double getNota() {
+        return nota;
+    }
 }
--- EjercicioFicherosAlumnos/src/main/java/uax/ListaDeAlumnos.java
+++ EjercicioFicherosAlumnos/src/main/java/uax/ListaDeAlumnos.java
@@ -10,6 +10,65 @@ public class ListaDeAlumnos {
         this.alumnos = new ArrayList<Alumno>();
     }
 
+    public void anadirAlumno(Alumno alumno){
+        alumnos.add(alumno);
+    }
+
+    public void bajaAlumno(int expediente){
+        Alumno alumnoBorrar = new Alumno();
+        for(Alumno alumno: alumnos){
+            if(alumno.getExpediente() == expediente){
+                alumnoBorrar = alumno;
+                break;
+            }
+        }
+        alumnos.remove(alumnoBorrar);
+    }
+
+    public void insertarNota(int expediente, double nota){
+        for(Alumno alumno: alumnos){
+            if(alumno.getExpediente() == expediente){
+                alumno.setNota(nota);
+                break;
+            }
+        }
+        System.out.println("Expediente no encontrado.");
+    }
+
+    public void modificarNota(int expediente, double nota){
+        for(Alumno alumno: alumnos){
+            if(alumno.getExpediente() == expediente){
+                alumno.setNota(nota);
+                break;
+            }
+        }
+        System.out.println("Expediente no encontrado.");
+    }
+
+    public void consultarNota(int expediente){
+        for(Alumno alumno: alumnos){
+            if(alumno.getExpediente() == expediente){
+                System.out.println("Nota: "+alumno.getNota());
+                break;
+            }
+        }
+
+        System.out.println("Expediente no encontrado.");
+    }
+
+    public void consultarTodasNotas(){
+        if(!alumnos.isEmpty()){
+            for(Alumno alumno: alumnos){
+                System.out.println("Alumno: "+alumno.getNombreYApellidos()+", Nota: "+ alumno.getNota());
+            }
+        }
+        else {
+            System.out.println("No hay alumnos");
+        }
+    }
+
+
+
 
 
 }
