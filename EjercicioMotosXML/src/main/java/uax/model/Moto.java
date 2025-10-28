6b47f5f77e37b76acfc70c7b69e9cc91b0d5e94c
DLizanaDev <Helgait@usal.es>
2025-10-28 08:44:43 +0100

    Created project for XML export and import exercise

---
+++ EjercicioMotosXML/src/main/java/uax/model/Moto.java
@@ -0,0 +1,15 @@
+package uax.model;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Moto {
+
+    private String marca;
+    private String modelo;
+    private String cilindrada;
+
+
+
+}
