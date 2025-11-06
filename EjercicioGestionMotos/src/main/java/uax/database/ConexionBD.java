50509e1eb879e056cc254d90986902f8bd15b151
DLizanaDev <Helgait@usal.es>
2025-11-06 18:11:15 +0100

    Added method to connection class

--- EjercicioGestionMotos/src/main/java/uax/database/ConexionBD.java
+++ EjercicioGestionMotos/src/main/java/uax/database/ConexionBD.java
@@ -1,6 +1,8 @@
 package uax.database;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexionBD {
     private static final String URL = "jdbc:mysql://localhost:3306/uax";
@@ -9,7 +11,14 @@ public class ConexionBD {
 
 
     public static Connection conectar(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL,USER,PASS);
 
+        }catch(SQLException | ClassNotFoundException e){
+            System.out.println("Error al conectar con la base de datos.");
+            return null;
+        }
     }
 
 }
